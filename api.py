49dd69c4638f8a8866396bfd97b416983471bb04
Neo Lee <neo.lee@gmail.com>
2024-10-08 16:23:03 +0800

    feat: better api endpoint wrapping and chat ui

---
+++ api.py
@@ -0,0 +1,43 @@
+from openai import OpenAI
+
+import json
+
+
+# init client point to the local server, api_key is irrelevant
+client = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+model_name = "llama3.2:1b"
+
+system_message_file = "system_message.txt"
+
+user_name = "Neo"
+agent_name = "Garfield"
+
+def load_system_message():
+    newline = '\n'
+    with open(system_message_file, 'r') as f:
+        system_message = f"{f.read().replace(newline, ' ')}".format(**globals())
+    return system_message
+
+def simple_chat_completion(q, sm):
+    return client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": sm},
+            {"role": "user", "content": q}
+        ],
+        temperature=0.7
+    )
+
+def chat_completion(stream, history):
+    return client.chat.completions.create(
+        model=model_name,
+        messages=history, # type: ignore
+        temperature=0.7,
+        stream=stream
+    )
+
+def stringify_history(history):
+    s = f"\n{'-'*20} History dump {'-'*20}\n"
+    s += json.dumps(history, indent=2)
+    s += f"\n{'-'*55}"
+    return s
--- chat.py
+++ chat.py
@@ -1,45 +1,41 @@
-from openai import OpenAI
-
-# init client point to the local server, api_key is irrelevant
-client = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
-
-user_name = "Neo"
-agent_name = "Garfield"
-
-
-def create_chat_completion(q, sm):
-    return client.chat.completions.create(
-        model="llama3.2:1b",
-        messages=[
-            {"role": "system", "content": sm},
-            {"role": "user", "content": q}
-        ],
-        temperature=0.7,
-    )
+from api import load_system_message
+from api import chat_completion
+from api import stringify_history
 
 
 def main():
-    # system_message = "Perform the task to the best of your ability."
-    system_message = f"""
-        "The user name is {user_name}. You are the assistant or chatbot and your name is {agent_name}. "
-        "Always refer to the user as {user_name}. Keep all responses brief and concise. "
-        "You, the assistant, are an expert in Artificial Intelligence, Machine Learning, Deep Learning, Generative AI, "
-        "Large Language Models, Transformers, Open Source LLMs, computer Science and Math.\n"
-        "Your primary job and role as the ASSISTANT is to help {user_name} learn, design, code and deploy his personal AI assistant. "
-        "We will accomplish this job by using open source LLMs, python libraries, TTS, STT, and Hugging Face and GitHub tools and resources.\n"
-        "Be concise and specific in responses. Avoid unnecessary details. Role-play accurately, understanding and mirroring user intent during scenarios.\n"
-        "Emphasize honesty, candor, and precision. Avoid speculation except when explicitly prompted to. "
-        "Maintain a friendly respectful and professional tone. Politely correct me if I am wrong and give evidence based facts. Never lecture me."
-    """
+    stream = True
+    verbose = False
+    welcome_message = "Introduce yourself to someone opening this program for the first time. Be concise."
+    system_message = load_system_message()
+    history = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": welcome_message},
+    ]
 
     while True:
-        q = input(f"{user_name}: ")
-        if q.lower() in ['exit', 'bye', 'quit', 'x', 'q']:
-            print("exiting the chat.")
-            break
+        new_message = {"role": "assistant", "content": ""}
+        completion = chat_completion(stream, history)
+        if stream:
+            for chunk in completion:
+                if chunk.choices[0].delta.content:
+                    print(chunk.choices[0].delta.content, end="", flush=True)
+                    new_message["content"] += chunk.choices[0].delta.content
+            print()
+        else:
+            response = completion.choices[0].message.content
+            print(response)
+            new_message["content"] = response
 
-        completion = create_chat_completion(q, system_message)
-        print(f"{agent_name}: ", completion.choices[0].message.content)
+        history.append(new_message)
+
+        # print history if in verbose mode
+        if verbose: print(stringify_history(history))
+        else: print()
+
+        q = input("> ")
+        if q.lower() in [':q', ':x', ':quit', ':exit', 'bye']: break
+        history.append({"role": "user", "content": q})
 
 
 if __name__ == "__main__":
